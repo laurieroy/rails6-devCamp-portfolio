dd5c8d4c47e76939a6303d1abbf56414f0b46217
Laurie Roy <laurieroy.dev@gmail.com>
2022-08-08 11:15:43 -0700

    Implement new, edit, add buttons to index

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,15 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, except: %i[index new create] 
+
   def index
     @portfolio_items = Portfolio.all
   end
 
   def new
     @portfolio_item = Portfolio.new
+  end
+
+  def edit
   end
 
   def create
@@ -21,9 +26,26 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to blog_url(@portfolio_item), notice: "Portfolio was successfully updated." }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private 
   
+  def set_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
   end
+
 end
