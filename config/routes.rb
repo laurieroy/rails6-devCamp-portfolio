1e33cc40682b91b233a38aa312408da4dce3baa7
Laurie Roy <laurieroy.dev@gmail.com>
2022-08-11 13:32:26 -0700

    Customize portfolios#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   resources :blogs
-  resources :portfolios
+  resources :portfolios, except: %i[show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
   get 'contact', to:'pages#contact'
