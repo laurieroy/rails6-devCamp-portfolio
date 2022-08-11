a7b7045218e82ea9c8ceb7d1fe8801f5ae1fa1ec
Laurie Roy <laurieroy.dev@gmail.com>
2022-08-11 15:36:28 -0700

    Add button to change blog status: draft, published

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
   
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   resources :portfolios, except: %i[show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
